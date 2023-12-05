18aa0e63c3b3e373f7eded1250084bc1570db4fd
Рэм Королев <remkorolev@MacBook-Pro-Rem.local>
2023-12-05 18:43:30 +0300

    first commit

---
+++ Task01.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const cart = {
+    items: [],
+    count: 0,
+
+    get totalPrice() {
+      return this.calculateItemPrice();
+    },
+  
+    calculateItemPrice() {
+      return this.items.reduce((total, item) => total + item.price * item.quantity, 0);
+    },
+  
+    increaseCount(number) {
+      this.count += number;
+    },
+  
+    add(name, price, quantity = 1) {
+      this.items.push({ name, price, quantity });
+      this.increaseCount(quantity);
+    },
+  
+    clear() {
+      this.items = [];
+      this.count = 0;
+    },
+  
+    print() {
+      console.log(JSON.stringify(this.items));
+      console.log(`Общая стоимость: ${this.totalPrice}`);
+    }
+};
+
+cart.add('Товар 1', 100, 2);
+cart.add('Товар 2', 200, 1);
+cart.add('Товар 3', 150);
+
+cart.print();
