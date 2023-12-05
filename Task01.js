2dfde33d4f83f6cb68ec2aa4c92cb85217447359
Рэм Королев <remkorolev@MacBook-Pro-Rem.local>
2023-12-05 22:48:50 +0300

    add ESlint

--- Task01.js
+++ Task01.js
@@ -1,35 +1,36 @@
-'use strict'
+'use strict';
 
 const cart = {
-    items: [],
-    count: 0,
+  items: [],
+  count: 0,
 
-    get totalPrice() {
-      return this.calculateItemPrice();
-    },
-  
-    calculateItemPrice() {
-      return this.items.reduce((total, item) => total + item.price * item.quantity, 0);
-    },
-  
-    increaseCount(number) {
-      this.count += number;
-    },
-  
-    add(name, price, quantity = 1) {
-      this.items.push({ name, price, quantity });
-      this.increaseCount(quantity);
-    },
-  
-    clear() {
-      this.items = [];
-      this.count = 0;
-    },
-  
-    print() {
-      console.log(JSON.stringify(this.items));
-      console.log(`Общая стоимость: ${this.totalPrice}`);
-    }
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+
+  calculateItemPrice() {
+    return this.items.reduce((total, item) =>
+      total + item.price * item.quantity, 0);
+  },
+
+  increaseCount(number) {
+    this.count += number;
+  },
+
+  add(name, price, quantity = 1) {
+    this.items.push({name, price, quantity});
+    this.increaseCount(quantity);
+  },
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
+
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`Общая стоимость: ${this.totalPrice}`);
+  },
 };
 
 cart.add('Товар 1', 100, 2);
